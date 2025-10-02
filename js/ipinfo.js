807b000738451eea6551fa4b68fe1c11da871afa
ZJK <zjinke666@gmail.com>
2025-10-02 14:03:20 +0800

    Update ipinfo.js

--- js/ipinfo.js
+++ js/ipinfo.js
@@ -62,6 +62,6 @@ let ip = obj["ip"];
 let country = countryMap[obj["country"]] || obj["country"];
 
 let title = " ";
-let subtitle = country + "-IP" + ip;
+let subtitle = country + "IP：" + ip;
 
 $done({ ip, title, subtitle });
